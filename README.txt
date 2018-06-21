fd26e3abdee7e9cb0025eeee5d392619f22eb346
Marisol Davila Escobar <marisol.davila@correounivalle.edu.co>
2018-06-21 14:32:36 -0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/soldavilaes/pen/wXmBEo.
+
+ Mobil Menu - Sidebar
---
+++ js/index.js
@@ -0,0 +1,8 @@
+(function(){
+  $('.menu-left').click(function(){
+    
+    $('#menu-left').toggleClass('active')
+   
+  })
+  $('.menu-right').click(function(){$('#menu-right').toggleClass('active')})
+})()
